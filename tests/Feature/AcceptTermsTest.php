6eb60f53613d47190c02bb969ae4e2858b921477
Duilio Palacios <duilio@styde.net>
2018-11-11 13:57:11 +0000

    Complete test

--- tests/Feature/AcceptTermsTest.php
+++ tests/Feature/AcceptTermsTest.php
@@ -11,8 +11,14 @@ class AcceptTermsTest extends TestCase
     /** @test */
     function a_user_can_accept_the_terms_and_conditions()
     {
+        $this->withoutExceptionHandling();
+
+        $this->post('accept-terms')
+            ->assertRedirect('/')
+            ->assertCookieMissing('accept_terms');
+
         $this->post('accept-terms', ['accept' => '1'])
             ->assertRedirect('/')
-            ->assertCookie('accept_terms', 1);
+            ->assertCookie('accept_terms', '1');
     }
 }
